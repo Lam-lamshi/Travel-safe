afcccfffe649d34b31ec4eff35c06e7faabc4f1f
Lam-lamshi <emmanuelwisdomolamide@gmail.com>
2025-12-18 19:34:22 +0100

    changes

--- search backend/backend.py
+++ search backend/backend.py
@@ -11,39 +11,61 @@ CORS(app)
 @app.route("/search", methods=["GET"])
 def search():
     query = request.args.get("q", "").lower().strip()
-    results=[]
 
     if not query:
         return jsonify({"results": []})
 
+    results = []
+    seen = set()
+
     for continent, countries in CONTINENTS.items():
         if query in continent:
             for country in countries:
-                results.append({
-                    "id":country["id"],
-                    "name":country["name"],
-                    "continents":continent
+                if country["id"] not in seen:
+                    results.append({
+                        "id":country["id"],
+                        "name":country["name"],
+                        "continent":continent
                 })
-                for country in countries:
-                    if query in country ["name"].lower():
-                        results.append({
-                             "id":country["id"],
-                                "name":country["name"],
-                                "continents":continent
-                            })
-                        return jsonify({"results":results})
+                seen.add(country["id"])
+    for country in countries:
+        if query in country ["name"].lower():
+            if country["id"] not in seen:
+                results.append({
+                     "id":country["id"],
+                    "name":country["name"],
+                     "continent":continent
+                })
+                seen.add(country["id"])
+
+    return jsonify({"results": results})
+
 
 
 
 NEWS_API_KEY = "70c71b55d502440784e247f0dc276fc8"
 @app.route("/news", methods=["GET"])
 def news():
-    url = (
-        f"https://newsapi.org/v2/everything?"
-        f"q=travel OR tourism&language=en&apiKey={NEWS_API_KEY}"
-    ) 
-    res = requests.get(url)
-    return jsonify(res.json())
+    try:
+        
+        url = "https://newsapi.org/v2/everything?"
+        params={
+            "q": "travel OR tourism",
+                
+                "language":"en",
+                "pageSize":10,
+                "apiKey": NEWS_API_KEY
+        }
+    
+        response=requests.get(url,params=params,timeout=10)
+        response.raise_for_status()
+
+        return jsonify(response.json())
+    except requests.exceptions.RequestException as e:
+        return jsonify({
+            "error": "Failed to fetch news articles",
+            "details": str(e)
+        }),500
 
 
 
